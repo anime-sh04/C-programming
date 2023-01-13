61cb09091d1a678f4b2174888433fa314eff1646
AnimeshRajpoot08 <114731278+AnimeshRajpoot08@users.noreply.github.com>
2023-01-13 12:25:39 +0530

    Create C program to print multiplication table of a number.c

---
+++ C program to print multiplication table of a number.c
@@ -0,0 +1,16 @@
+18------
+#include <stdio.h>
+
+int main()
+{
+    int i, num;
+    printf("Enter number to print table: ");
+    scanf("%d", &num);
+
+    for(i=1; i<=10; i++)
+    {
+        printf("%d * %d = %d\n", num, i, (num*i));
+    }
+
+    return 0;
+}
