dfc25ce845928d17f28d55252ca51710fe43da71
AnimeshRajpoot08 <114731278+AnimeshRajpoot08@users.noreply.github.com>
2023-01-13 14:19:16 +0530

    Create C program to add two number using pointers.c

---
+++ C program to add two number using pointers.c
@@ -0,0 +1,13 @@
+48—
+#include <stdio.h>
+int main()
+{
+int first, second, *p, *q, sum;
+printf("Enter two integers to add\n");
+scanf("%d%d", &first, &second);
+p = &first;
+q = &second;
+sum = *p + *q;
+printf("Sum of the numbers = %d\n", sum);
+return 0;
+}
