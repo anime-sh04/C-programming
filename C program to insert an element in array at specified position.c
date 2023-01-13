f18182d1636afbb055367244507944a823d57215
AnimeshRajpoot08 <114731278+AnimeshRajpoot08@users.noreply.github.com>
2023-01-13 12:39:41 +0530

    Create C program to insert an element in array at specified position.c

---
+++ C program to insert an element in array at specified position.c
@@ -0,0 +1,22 @@
+33—
+#include <stdio.h>
+int main()
+{
+int arr[100] = { 0 };
+int i, x, pos, n = 10;
+for (i = 0; i < 10; i++)
+arr[i] = i + 1;
+for (i = 0; i < n; i++)
+printf("%d ", arr[i]);
+printf("\n");
+x = 50;
+pos = 5;
+n++;
+for (i = n - 1; i >= pos; i--)
+arr[i] = arr[i - 1];
+arr[pos - 1] = x;
+for (i = 0; i < n; i++)
+printf("%d ", arr[i]);
+printf("\n");
+return 0;
+}
