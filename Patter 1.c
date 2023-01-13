ea33dec170b12caac11694540e420e8798cd4569
AnimeshRajpoot08 <114731278+AnimeshRajpoot08@users.noreply.github.com>
2023-01-13 14:22:12 +0530

    Create Patter 1.c

---
+++ Patter 1.c
@@ -0,0 +1,17 @@
+51—
+#include <stdio.h>
+int main()
+{
+int i, j, N;
+printf("Enter number of rows: ");
+scanf("%d", &N);
+for(i=1; i<=N; i++)
+{
+for(j=1; j<=N; j++)
+{
+printf("*");
+}
+printf("\n");
+}
+return 0;
+}
