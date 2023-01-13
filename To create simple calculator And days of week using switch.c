8e6c5fee2414a6dc98585f5322b481373145696a
AnimeshRajpoot08 <114731278+AnimeshRajpoot08@users.noreply.github.com>
2023-01-13 12:19:24 +0530

    Create To create simple calculator And days of week using switch.c

---
+++ To create simple calculator And days of week using switch.c
@@ -0,0 +1,69 @@
+12--
+#include <stdio.h>
+
+int main() {
+
+  char op;
+  double first, second;
+  printf("Enter an operator (+, -, *, /): ");
+  scanf("%c", &op);
+  printf("Enter two operands: ");
+  scanf("%lf %lf", &first, &second);
+
+  switch (op) {
+    case '+':
+      printf("%.1lf + %.1lf = %.1lf", first, second, first + second);
+      break;
+    case '-':
+      printf("%.1lf - %.1lf = %.1lf", first, second, first - second);
+      break;
+    case '*':
+      printf("%.1lf * %.1lf = %.1lf", first, second, first * second);
+      break;
+    case '/':
+      printf("%.1lf / %.1lf = %.1lf", first, second, first / second);
+      break;
+    default:
+      printf("Error! operator is not correct");
+  }
+          AND
+#include <stdio.h>
+
+int main()
+{
+    int week;
+    printf("Enter week number(1-7): ");
+    scanf("%d", &week);
+    
+    switch(week)
+    {
+        case 1: 
+            printf("Monday");
+            break;
+        case 2: 
+            printf("Tuesday");
+            break;
+        case 3: 
+            printf("Wednesday");
+            break;
+        case 4: 
+            printf("Thursday");
+            break;
+        case 5: 
+            printf("Friday");
+            break;
+        case 6: 
+            printf("Saturday");
+            break;
+        case 7: 
+            printf("Sunday");
+            break;
+        default: 
+            printf("Invalid input! Please enter week number between 1-7.");
+    }
+
+    return 0;
+}
+
+  return 0;
+}
