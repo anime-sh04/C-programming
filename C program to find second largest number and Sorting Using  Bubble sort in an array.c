de4c9814090a3513435c52be018e3f979204cdee
AnimeshRajpoot08 <114731278+AnimeshRajpoot08@users.noreply.github.com>
2023-01-13 12:42:13 +0530

    Create C program to find second largest number and Sorting Using  Bubble sort in an array.c

---
+++ C program to find second largest number and Sorting Using  Bubble sort in an array.c
@@ -0,0 +1,25 @@
+36—
+#include <stdio.h>
+void main()
+{
+int a[100],i,j,n,temp;
+printf ("Enter the number of elements:");
+scanf ("%d",&n);
+printf("Enter the values:");
+for (i=0;i<n;i++){
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+if(a[i]>a[j])
+{
+temp = a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+printf("Second largest element:%d",a[n-2]);
+}
