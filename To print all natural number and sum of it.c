99311c4f01a5aab41175279938fa9cd992c4c5a8
AnimeshRajpoot08 <114731278+AnimeshRajpoot08@users.noreply.github.com>
2023-01-13 12:23:41 +0530

    Create To print all natural number and sum of it.c

---
+++ To print all natural number and sum of it.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, n, sum=0;
+
+    /* Input upper limit from user */
+    printf("Enter upper limit: ");
+    scanf("%d", &n);
+    for(i=1; i<=n; i++)
+    {
+        sum += i;
+    }
+
+    printf("Sum of first %d natural numbers = %d", n, sum);
+
+    return 0;
+}
